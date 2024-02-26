82fe8df9516304fabf6c8686cb02fc7da5583f23
Zeeshan Zahid <ZeeshanZahid663@gmail.com>
2024-02-26 19:19:30 +0500

    Version 1 (UI Design)

---
+++ OnlineRailwayReservationSystem/Controllers/AdminController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineRailwayReservationSystem.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Users()
+        {
+            return View();
+        }
+        public IActionResult StationMaster()
+        {
+            return View();
+        }
+        public IActionResult StationMasterEdit()
+        {
+            return View();
+        }
+        public IActionResult TrainMaster()
+        {
+            return View();
+        }
+        public IActionResult TrainMasterEdit()
+        {
+            return View();
+        }
+        public IActionResult TrainScheduleMaster()
+        {
+            return View();
+        }
+        public IActionResult TrainScheduleMasterEdit()
+        {
+            return View();
+        }
+        public IActionResult FareRule()
+        {
+            return View();
+        }
+        public IActionResult CancellationFees()
+        {
+            return View();
+        }
+        public IActionResult ReservationFees()
+        {
+            return View();
+        }
+        public IActionResult Reservation()
+        {
+            return View();
+        }
+        public IActionResult ReservationBooking()
+        {
+            return View();
+        }
+        public IActionResult ReservationBookingConfirmation()
+        {
+            return View();
+        }
+        public IActionResult Cancellation()
+        {
+            return View();
+        }
+        public IActionResult Transaction()
+        {
+            return View();
+        }
+        public IActionResult PNRInquiry()
+        {
+            return View();
+        }
+    }
+}
